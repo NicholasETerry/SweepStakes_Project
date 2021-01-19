2dcb87828afafa3fbae8fab84d1ea56fccc8e68b
NicholasETerry <nicholas.e.terry@gmail.com>
2021-01-18 21:25:50 -0600

    1. Added methods in UserInterface to get user first, last, and email. 2. Created check method shells to confirm user input is valid 3. assigned contestant variables to the values UserInterface methods return

--- SweepStakesProject/Contestants.cs
+++ SweepStakesProject/Contestants.cs
@@ -8,10 +8,25 @@ namespace SweepStakesProject
 {
     class Contestants
     {
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public int RegistrationNumber;
+        // MEMBER VARIABLES ( HAVE A )
         // As a developer i want to create a Contestant class that has
         // 1. first name
         // 2. last name
         // 3. email address
         // 4. registration number
+        //PROPERTIES (GET:SET)
+        // CONSTRUCTOR
+        public Contestants()
+        {
+            FirstName = UserInterface.CustomerFirstName();
+            LastName = UserInterface.CustomerLastName();
+            EmailAddress = UserInterface.CustomerEmailAddress();
+            RegistrationNumber = UserInterface.RegistrationNumber();
+        }
+        // METHODS ( HAVE A )
     }
 }
--- SweepStakesProject/UserInterface.cs
+++ SweepStakesProject/UserInterface.cs
@@ -6,9 +6,42 @@ using System.Threading.Tasks;
 
 namespace SweepStakesProject
 {
-    class UserInterface
+    static class UserInterface // made static !
     {
+        //MEMBER VARIABLES ( HAVE A )
         // Create a user interface for any information the application would need to get from the user. One example would be the functionality
         // to assign a Contestant object a first name, last name, email address, and registration number
+        // PROPERTIES(GET:SET)
+
+        // CONSTRUCTOR
+
+        // METHODS ( CAN DO )
+        public static string CustomerFirstName()
+        {
+            Console.WriteLine("Enter Contestant's First Name:");
+            Confirm(Console.ReadLine());
+        }
+        public static string CustomerLastName()
+        {
+            Console.WriteLine("Enter Contestant's Last Name:");
+            Confirm(Console.ReadLine());
+        }
+        public static string CustomerEmailAddress()
+        {
+            Console.WriteLine("Enter Contestant's Email Address:");
+            ConfirmEmail(Console.ReadLine());
+        }
+        public static int RegistrationNumber()
+        {
+
+        }
+        private static bool Confirm(string toConfirm)
+        {
+
+        }
+        private static bool ConfirmEmail(string EmailToConfirm)
+        {
+
+        }
     }
 }
